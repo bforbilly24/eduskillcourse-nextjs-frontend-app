09579599a491ec7d22723859c6b14792ea637a49
Halim <halimputra0701@gmail.com>
2024-11-05 17:59:18 +0700

    refactor(action): update data from fetching /status

--- src/actions/get-server-status.js
+++ src/actions/get-server-status.js
@@ -6,16 +6,31 @@ async function getServerstatus() {
 		cache: 'no-store',
 	};
 
-	const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/status`;
-
 	try {
-		const res = await fetch(apiUrl, options);
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/status`, options);
+
+		if (!res.ok) {
+			throw new Error('Failed to fetch server status');
+		}
+
 		const data = await res.json();
 
-		return data;
+		const status = data.status;
+		const cpuUsage = data.details.cpu_usage;
+		const memoryUsage = data.details.memory_usage;
+		const diskFreeSpace = data.details.disk_free_space;
+		const diskTotalSpace = data.details.disk_total_space;
+
+		return {
+			status,
+			cpuUsage,
+			memoryUsage,
+			diskFreeSpace,
+			diskTotalSpace
+		};
 	} catch (error) {
 		console.error('Error fetching server status:', error);
-		return null;
+		return { error: 'Unable to fetch server status' };
 	}
 }
 

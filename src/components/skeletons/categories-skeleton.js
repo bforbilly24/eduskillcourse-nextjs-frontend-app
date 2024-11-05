c1bbf0068ac4c9e56b5e36fbfdff7a836da3dd22
Halim <halimputra0701@gmail.com>
2024-11-05 18:05:50 +0700

    style(component): update color background for darkmode

--- src/components/skeletons/categories-skeleton.js
+++ src/components/skeletons/categories-skeleton.js
@@ -3,7 +3,7 @@ function CategoriesSkeleton() {
 		<div className='w-full flex flex-col gap-y-10 overflow-hidden'>
 			<div className='flex gap-x-10'>
 				{[...Array(3)].map((_, index) => (
-					<div key={index} className='w-[24.375rem] h-[15.188rem] bg-white dark:bg-gray-800 animate-pulse border-2 border-[#D8D8D8]/40 rounded-[0.75rem] flex-shrink-0'>
+					<div key={index} className='w-[24.375rem] h-[15.188rem] bg-white dark:bg-gray-700 animate-pulse border-2 border-[#D8D8D8]/40 rounded-[0.75rem] flex-shrink-0'>
 						<div className='flex flex-col gap-y-5 w-full h-full justify-between p-8'>
 							<div className='bg-gray-200 dark:bg-gray-700 h-8 w-2/3 rounded-md'></div>
 							<div className='bg-gray-100 dark:bg-gray-600 h-4 w-1/2 rounded-md'></div>
